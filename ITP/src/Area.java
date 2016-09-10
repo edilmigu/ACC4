c1d587fd40ded56fb11497338546f725dbc6793a
pi <peter@isburgh.com>
2016-09-09 23:18:26 -0500

    class Area assignment Sep 8

---
+++ ITP/src/Area.java
@@ -0,0 +1,25 @@
+/**
+ * Write a class called Area that takes two values from the command line 
+ * arguments (length and width) and computes the area of a rectangle.
+ * 
+ * Arguments are set on the Run Configurations/Arguments tab.
+ * 
+ * The trick is to convert the input values which are type String into 
+ * actual integers to do arithmetic with them. Use Google searching for
+ * "how to convert java string to integer".
+ * 
+ * @author pi
+ *
+ */
+public class Area {
+	
+	public static void main(String[] dims) {
+		// Here's the plan:
+		//   Get each value of dims into a variable i.e. 
+		String length = dims[0];
+		String width = dims[1];
+		//   Missing: Covert length and width into actual integers and multiply
+		//   Missing: Print the area followed by the word "inches"
+	}
+
+}
